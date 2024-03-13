21269b38445a7d709a47e6bc1f9e1d90a040ecf7
Lopes30787 <afonsomlopes2001@gmail.com>
2024-03-13 19:01:16 +0000

    Model Saving code

--- fine-tuning.py
+++ fine-tuning.py
@@ -168,3 +168,25 @@ for epoch in range(num_epochs):
 
     results = metric.compute()
     print(f"epoch {epoch}, BLEU score: {results['score']:.2f}")
+
+# ======================================== #
+#                 Saving                   #
+# ======================================== #
+
+output_dir = './model_save/'
+
+# Create output directory if needed
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+print("Saving model to %s" % output_dir)
+
+# Save a trained model, configuration and tokenizer using `save_pretrained()`.
+# They can then be reloaded using `from_pretrained()`
+model_to_save = model.module if hasattr(model, 'module') else model  # Take care of distributed/parallel training
+model_to_save.save_pretrained(output_dir)
+tokenizer.save_pretrained(output_dir)
+
+# Save training arguments
+# TODO: Put arguments into a variable in order to be easy to save
+#torch.save(args, os.path.join(output_dir, 'training_args.bin'))
