2ef38f5de1170d3864788082002d569c6e712cef
Lopes30787 <afonsomlopes2001@gmail.com>
2024-03-16 21:48:10 +0000

    accelerator

--- fine-tuning.py
+++ fine-tuning.py
@@ -85,14 +85,14 @@ test_dataloader = DataLoader(
 # Define optimizer and learning rate
 optimizer = torch.optim.AdamW(model.parameters(), lr=2e-5)
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-model.to(device)
-print(f"Using device: {device}")
+#device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+#model.to(device)
+#print(f"Using device: {device}")
 # Define Accelerator
-#accelerator = Accelerator()
-#model, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(
-#    model, optimizer, train_dataloader, test_dataloader
-#)
+accelerator = Accelerator()
+model, optimizer, train_dataloader, eval_dataloader = accelerator.prepare(
+    model, optimizer, train_dataloader, test_dataloader
+)
 
 # Define learning rate scheduler
 num_epochs = 3
@@ -135,14 +135,14 @@ for epoch in range(num_epochs):
     model.train()
 
     for batch in train_dataloader:
-        batch = {k: v.to(device) for k, v in batch.items()}
+        #batch = {k: v.to(device) for k, v in batch.items()}
         outputs = model(**batch)
         
         # Obtain Loss
         loss = outputs[0]
     
-        #accelerator.backward(loss)
-        loss.backward()
+        accelerator.backward(loss)
+        #loss.backward()
 
         optimizer.step()
         lr_scheduler.step()
