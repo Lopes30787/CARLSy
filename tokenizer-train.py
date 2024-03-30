8dfbc1f8d11239bb4799ad4c4a0df8a971b4642d
Lopes30787 <afonsomlopes2001@gmail.com>
2024-03-30 14:47:44 +0000

    new tokenizer

--- tokenizer-train.py
+++ tokenizer-train.py
@@ -14,18 +14,18 @@ os.environ["TOKENIZERS_PARALLELISM"]= "false"
 old_tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
 
 # Put the dataset in a Pandas DataFrame
-df = pd.read_csv('/cfs/home/u024219/Tese/CARLSy/chess_dataset.csv', sep='|', skipinitialspace= True, encoding_errors='ignore')
+df = pd.read_csv('/cfs/home/u024219/Tese/CARLSy/datasets/chess_dataset_extended_with_move.csv', sep='|', skipinitialspace= True, encoding_errors='ignore')
 #df = pd.read_csv('C:\\Users\\afons\\Ambiente de Trabalho\\dataset\\chess_dataset.csv', sep='|', skipinitialspace= True, encoding_errors='ignore')
 df = pd.DataFrame(df)
 
-df['Notation:Commentary'] = df['algebraic_notation'] + ": " + df['commentary']
+df['Training'] ="[PGN]" + df['algebraic_notation'] + ["MOVE"] + df['move'] + "[BOARD]" + df['positions'] + "[ATTACKS] " + df['attacks'] + "[COMMENTARY]" + df['commentary']
 
 # Load Dataset from Pandas DataFrame
 chess_dataset = Dataset.from_pandas(df)
 
 def get_training_corpus():
     return (
-        chess_dataset[i : i + 1000]["Notation:Commentary"]
+        chess_dataset[i : i + 1000]["Training"]
         for i in range(0, len(chess_dataset), 1000)
     )
 
